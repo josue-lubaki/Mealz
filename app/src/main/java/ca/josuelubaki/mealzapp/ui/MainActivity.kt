cd4efbc1006f7461dcffb94a14cb36bbe33d24fc
Josue Lubaki <josuelubaki@gmail.com>
2023-01-02 18:43:53 -0500

    Customizing the Meals Screen

--- app/src/main/java/ca/josuelubaki/mealzapp/ui/MainActivity.kt
+++ app/src/main/java/ca/josuelubaki/mealzapp/ui/MainActivity.kt
@@ -1,22 +1,14 @@
-package ca.josuelubaki.mealzapp.ui.meals
+package ca.josuelubaki.mealzapp.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+import ca.josuelubaki.mealzapp.ui.meals.MealsCategoriesScreen
 import ca.josuelubaki.mealzapp.ui.theme.MealzAppTheme
-import ca.josuelubaki.model.response.MealResponse
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,25 +24,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun MealsCategoriesScreen() {
-    val viewModel : MealCategoriesViewModel = viewModel()
-    val meals = viewModel.mealsState.value
-
-    LazyColumn {
-        items(meals) { meal ->
-            Text(text = meal.name)
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MealzAppTheme {
-        MealsCategoriesScreen()
     }
 }
---
+++ app/src/main/java/ca/josuelubaki/mealzapp/ui/meals/MealsCategoriesScreen.kt
@@ -0,0 +1,77 @@
+package ca.josuelubaki.mealzapp.ui.meals
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ca.josuelubaki.mealzapp.ui.theme.MealzAppTheme
+import ca.josuelubaki.model.response.MealResponse
+import coil.compose.rememberAsyncImagePainter
+
+
+@Composable
+fun MealsCategoriesScreen() {
+    val viewModel : MealCategoriesViewModel = viewModel()
+    val meals = viewModel.mealsState.value
+
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+    ) {
+        items(meals) { meal ->
+            MealCategory(meal)
+        }
+    }
+}
+
+@Composable
+fun MealCategory(meal : MealResponse) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 2.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
+    ) {
+        Row {
+
+            Image(
+                painter = rememberAsyncImagePainter(meal.imageUrl),
+                contentDescription = "Meal Picture",
+                modifier = Modifier
+                    .size(88.dp)
+                    .padding(4.dp),
+                contentScale = ContentScale.Inside,
+            )
+
+            Column(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(start = 16.dp)
+            ) {
+                Text(
+                    text = meal.name,
+                    style = MaterialTheme.typography.h6
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MealzAppTheme {
+        MealsCategoriesScreen()
+    }
+}
