561d22c6f7f1d11fd3d9c4fb8af428a5a2e4ca65
Josue Lubaki <josuelubaki@gmail.com>
2023-01-02 20:12:15 -0500

    implementing expanding list items

--- app/src/main/java/ca/josuelubaki/mealzapp/ui/meals/MealsCategoriesScreen.kt
+++ app/src/main/java/ca/josuelubaki/mealzapp/ui/meals/MealsCategoriesScreen.kt
@@ -1,17 +1,22 @@
 package ca.josuelubaki.mealzapp.ui.meals
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -36,6 +41,8 @@ fun MealsCategoriesScreen() {
 
 @Composable
 fun MealCategory(meal : MealResponse) {
+    var isExpanded by remember { mutableStateOf(false) }
+
     Card(
         shape = RoundedCornerShape(8.dp),
         elevation = 2.dp,
@@ -43,27 +50,49 @@ fun MealCategory(meal : MealResponse) {
             .fillMaxWidth()
             .padding(top = 16.dp)
     ) {
-        Row {
+        Row(
+            modifier = Modifier.animateContentSize()
+        ) {
 
             Image(
                 painter = rememberAsyncImagePainter(meal.imageUrl),
                 contentDescription = "Meal Picture",
                 modifier = Modifier
                     .size(88.dp)
-                    .padding(4.dp),
+                    .padding(4.dp)
+                    .align(Alignment.CenterVertically),
                 contentScale = ContentScale.Inside,
             )
 
             Column(
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
-                    .padding(start = 16.dp)
+                    .fillMaxWidth(0.8f)
+                    .padding(16.dp)
             ) {
                 Text(
                     text = meal.name,
                     style = MaterialTheme.typography.h6
                 )
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(
+                        text = meal.description,
+                        textAlign = TextAlign.Start,
+                        style = MaterialTheme.typography.subtitle2,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = if(isExpanded) 10 else 4
+                    )
+                }
             }
+
+            Icon(
+                imageVector = if(isExpanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                contentDescription = "Expand row icon",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(if(isExpanded) Alignment.Bottom else Alignment.CenterVertically)
+                    .clickable { isExpanded = !isExpanded }
+            )
         }
     }
 }
